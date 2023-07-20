7149cee92f25341462da479033ce2293f9cb73c1
SeokWon <tjrdnjs1534@naver.com>
2023-07-20 15:07:32 +0900

    ts initialize

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+
+class App {
+  public application: express.Application;
+
+  constructor() {
+    this.application = express();
+  }
+}
+
+const app = new App().application;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log("Listen on port ${PORT}"));
