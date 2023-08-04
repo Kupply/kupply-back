28cbb2e95bd0d5bb2b93fea7d7d80a0514a9c600
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-08-04 18:30:27 +0900

    fix: major controller, service 구조 변경

--- src/service/majorService.ts
+++ src/service/majorService.ts
@@ -1,70 +1,44 @@
-import { NextFunction, Request, Response } from 'express';
-import Major from '../models/majorModel';
+import Major, { IMajor } from '../models/majorModel';
 
-export const getAllMajors = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const majors = await Major.find();
-    return majors;
-  } catch (err) {
-    next(err);
-  }
+export const getAllMajors = async () => {
+  const majors = await Major.find();
+  return majors;
 };
 
-export const createMajor = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const major = await Major.create(req.body);
-    return major;
-  } catch (err) {
-    next(err);
-  }
+export const createMajor = async (majorData: IMajor) => {
+  const major = await Major.create(majorData);
+  return major;
 };
 
-export const getMajor = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const major = await Major.findById(req.params.id);
-    return major;
-  } catch (err) {
-    next(err);
+export const getMajor = async (majorId: string) => {
+  const major = await Major.findById(majorId);
+
+  if (!major) {
+    throw { status: 404, message: 'No major found with this id' };
   }
+
+  return major;
 };
 
-export const updateMajor = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const major = await Major.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    return major;
-  } catch (err) {
-    next(err);
+export const updateMajor = async (majorId: string, majorData: IMajor) => {
+  const major = await Major.findByIdAndUpdate(majorId, majorData, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!major) {
+    throw { status: 404, message: 'No major found with this id' };
   }
+
+  return major;
 };
 
-export const deleteMajor = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const major = await Major.findByIdAndDelete(req.params.id);
-    return major;
-  } catch (err) {
-    next(err);
+export const deleteMajor = async (majorId: string) => {
+  const major = await Major.findByIdAndDelete(majorId);
+
+  if (!major) {
+    throw { status: 404, message: 'No major found with this id' };
   }
+
+  return;
 };
