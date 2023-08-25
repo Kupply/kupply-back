7e6dc84c14709268c1dcd6d7bd662053f0bedf9d
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-08-25 20:55:24 +0900

    refactor: auth router

---
+++ src/router/authRouter.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import * as authController from '../controller/authController';
+
+const router = express.Router();
+
+router.post('/join', authController.join);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+router.post('/sendEmail', authController.sendEmail);
+router.post('/certifyEmail', authController.certifyEmail);
+
+export default router;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,22 @@
 import express from 'express';
+import authRouter from './authRouter';
 import userRouter from './userRouter';
 import majorRouter from './majorRouter';
 import dashboardRouter from './dashboard';
 import postRouter from './postRouter';
 import messageRouter from './messageRouter';
-import pastDataRouter from './pastDataRouter'
+import pastDataRouter from './pastDataRouter';
 import * as authController from '../controller/authController';
 
 const router = express.Router();
 
-router.post('/join', authController.join);
-router.post('/login', authController.login);
-router.get('/logout', authController.logout);
-router.get('/certify/:certificateToken', authController.certifyUser);
+router.use('/auth', authRouter);
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// FIXME: 나중에 적절한 위치로 수정
 router.use(authController.protect);
 
 router.use('/user', userRouter);
