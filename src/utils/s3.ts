5665744a865fab2c1e3abb5524627457cd9e083f
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-10-08 14:12:10 +0900

    feat: connection with AWS S3

---
+++ src/utils/s3.ts
@@ -0,0 +1,62 @@
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import multer from 'multer';
+
+const s3AccessKey = process.env.S3_ACCESS_KEY as string;
+const s3SecretKey = process.env.S3_SECRET_KEY as string;
+const bucketName = process.env.BUCKET_NAME as string;
+const bucketRegion = process.env.BUCKET_REGION as string;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: s3AccessKey,
+    secretAccessKey: s3SecretKey,
+  },
+  region: bucketRegion,
+});
+
+export const upload = multer({
+  storage: multer.memoryStorage(),
+});
+
+export const getFileFromS3 = async (getObjectParams: { Key: string }) => {
+  const command = new GetObjectCommand({
+    ...getObjectParams,
+    Bucket: bucketName,
+  });
+
+  const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+  return url;
+};
+
+export const uploadFileToS3 = async (uploadObjectParams: {
+  Key: string;
+  Body: Buffer;
+  ContentType: string;
+}) => {
+  const command = new PutObjectCommand({
+    ...uploadObjectParams,
+    Bucket: bucketName,
+  });
+
+  await s3.send(command);
+
+  return;
+};
+
+export const deleteFileFromS3 = async (deleteObjectParams: { Key: string }) => {
+  const command = new DeleteObjectCommand({
+    ...deleteObjectParams,
+    Bucket: bucketName,
+  });
+
+  await s3.send(command);
+
+  return;
+};
