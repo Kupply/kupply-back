c6da2856cdea10d4b600e9de7da767b2e64a4cd6
YoonJunHyeok <jhyoon1607@gmail.com>
2024-03-21 02:47:40 +0900

    re

--- src/service/pastDataService.ts
+++ src/service/pastDataService.ts
@@ -141,18 +141,18 @@ type MajorOptions =
   | 'chemistry'
   | 'media'
   | 'foodecon'
-  | 'computer'
-  | 'bioeng'
-  | 'lifesci'
-  | 'political'
-  | 'pubadmin'
-  | 'materials'
-  | 'mechanical'
-  | 'industrial'
-  | 'electrical'
-  | 'chembio'
-  | 'datasci'
-  | 'smartsec';
+  | 'computer';
+// | 'bioeng'
+// | 'lifesci'
+// | 'political'
+// | 'pubadmin'
+// | 'materials'
+// | 'mechanical'
+// | 'industrial'
+// | 'electrical'
+// | 'chembio'
+// | 'datasci'
+// | 'smartsec';
 
 const majorNameMapping = {
   business: '경영학과',
@@ -164,17 +164,17 @@ const majorNameMapping = {
   media: '미디어학부',
   foodecon: '식품자원경제학과',
   computer: '컴퓨터학과',
-  bioeng: '생명공학과',
-  lifesci: '생명과학부',
-  political: '정치외교학과',
-  pubadmin: '행정학과',
-  materials: '신소재공학부',
-  mechanical: '기계공학부',
-  industrial: '산업경영공학부',
-  electrical: '전기전자공학부',
-  chembio: '화공생명공학부',
-  datasci: '데이터과학과',
-  smartsec: '스마트보안학부',
+  // bioeng: '생명공학과',
+  // lifesci: '생명과학부',
+  // political: '정치외교학과',
+  // pubadmin: '행정학과',
+  // materials: '신소재공학부',
+  // mechanical: '기계공학부',
+  // industrial: '산업경영공학부',
+  // electrical: '전기전자공학부',
+  // chembio: '화공생명공학부',
+  // datasci: '데이터과학과',
+  // smartsec: '스마트보안학부',
 };
 
 type GPAData = {
