be4168c90567b871898435fbf66c361f79edac29
YoonJunHyeok <jhyoon1607@gmail.com>
2024-06-30 18:24:22 +0900

    fix: return string format

--- src/utils/semester.ts
+++ src/utils/semester.ts
@@ -4,8 +4,8 @@ export const getCurrentSemester = (): string => {
   const month = now.getMonth() + 1; // getMonth() returns month from 0-11
 
   if (month <= 8) {
-    return `${year}_1`;
+    return `${year}-1`;
   } else {
-    return `${year}_2`;
+    return `${year}-2`;
   }
 };
