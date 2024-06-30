d57ffda8048bb099119884064bd52216648d68ce
YoonJunHyeok <jhyoon1607@gmail.com>
2024-06-30 21:23:06 +0900

    feat: get previous semester

--- src/utils/semester.ts
+++ src/utils/semester.ts
@@ -9,3 +9,15 @@ export const getCurrentSemester = (): string => {
     return `${year}-2`;
   }
 };
+
+export const getPrevSemester = (): string => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1; // getMonth() returns month from 0-11
+
+  if (month <= 8) {
+    return `${year - 1}-2`;
+  } else {
+    return `${year}-1`;
+  }
+};
