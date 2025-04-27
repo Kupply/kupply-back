079b4d915963f1305ef3617294c21737ee7aefc1
YoonJunHyeok <jhyoon1607@gmail.com>
2025-04-27 15:18:47 +0900

    Fix: delete campus from major and required false for campus

--- src/models/majorModel.ts
+++ src/models/majorModel.ts
@@ -6,7 +6,6 @@ export interface IMajor {
   _id: Types.ObjectId;
   name: string; // 학과 한글 이름
   code: string; // 고파스 학과 코드
-  campus: string; // 소속캠퍼스 (서울캠: A , 세종캠: S , 대학원생: G, 교류학생: C)
   collegeName: string; // 단과대 한글 이름
   shortEngName: string; // 학과 영문 약어
   longEngName: string; // 학과 영문 이름
@@ -26,13 +25,8 @@ const majorSchema = new Schema<IMajor>({
     type: String,
     required: true,
   },
-  campus: {
-    type: String,
-    required: true,
-  },
   collegeName: {
     type: String,
-    required: true,
   },
   shortEngName: {
     type: String,
